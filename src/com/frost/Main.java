4e1b72a9b168a48ca4be81530c28c9f891a22063
frost8111981 <73012443+frost8111981@users.noreply.github.com>
2021-03-08 15:38:48 +0300

    last

--- src/com/frost/Main.java
+++ src/com/frost/Main.java
@@ -10,22 +10,19 @@ import java.util.Map;
 public class Main {
     public static void main(String[] args) throws IOException {
         Map<String, User> map = new HashMap<>();
-
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        User yarik = new User("Ярик", "Мартынов", "Вячеславович", "Программист", 29);
+        User tima = new User("Тима", "Юлдашев", "Альбертович", "Сеньер Developer", 39);
+        User dima = new User("Дима", "Токарь", "Андреевич", " Супер Сеньер Developer", 29);
+        User kirill = new User("Кирилл", "Власкин", "Викторович", "ИТ", 27);
+        User ilya = new User("Илья", "Куравцев", "Сергеевич", "Администратор", 27);
+        map.put(yarik.surname, yarik);
+        map.put(tima.surname, tima);
+        map.put(dima.surname, dima);
+        map.put(kirill.surname, kirill);
+        map.put(ilya.surname, ilya);
 
         while (true) {
-            BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-            User yarik = new User("Ярик", "Мартынов", "Вячеславович", "Программист", 29);
-            User tima = new User("Тима", "Юлдашев", "Альбертович", "Сеньер Developer", 39);
-            User dima = new User("Дима", "Токарь", "Андреевич", " Супер Сеньер Developer", 29);
-            User kirill = new User("Кирилл", "Власкин", "Викторович", "ИТ", 27);
-            User ilya = new User("Илья", "Куравцев", "Сергеевич", "Администратор", 27);
-
-
-            map.put(yarik.surname, yarik);
-            map.put(tima.surname, tima);
-            map.put(dima.surname, dima);
-            map.put(kirill.surname, kirill);
-            map.put(ilya.surname, ilya);
             System.out.println();
             System.out.println();
             System.out.println("МЕНЮ - Выберите действие.");
@@ -42,7 +39,6 @@ public class Main {
                     System.out.println(map.get(inputUser));
                     break;
                 case 2:
-
                     System.out.println("Введите Имя сотрудника ");
                     String name = reader.readLine();
                     System.out.println("Введите Фамилию сотрудника");
@@ -56,15 +52,12 @@ public class Main {
                     User default1 = new User(name, surname, patronymic, prifession, age);
                     map.put(default1.surname, default1);
                     break;
-
                 case 3:
                     System.out.println("Введите фамилию сотрудника для удаления");
                     String del = reader.readLine();
                     map.remove(del);
                     System.out.println("Сотрудник " + del + " удален.");
                     break;
-
-
                 case 4:
                     for (Map.Entry<String, User> entry : map.entrySet()) {
                         System.out.println(entry.getKey());
